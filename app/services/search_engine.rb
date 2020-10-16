357a9d92c9f335ea1eac1cef473180debb63366f
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 15:57:22 -0500

    search_error & valid_url?

--- app/services/search_engine.rb
+++ app/services/search_engine.rb
@@ -14,4 +14,16 @@ class SearchEngine
   def self.name
     'SearchEngine'
   end
+
+  def search_error(error)
+    { 'error': 'An error ocurred through the search process',
+      'details': error }
+  end
+
+  def valid_url?(url)
+    uri = URI.parse(URI.escape(url))
+    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue StandardError
+    false
+  end
 end
