bba36135d26e338a28128c51a40991d821e04029
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 06:18:45 -0500

    added services to eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -24,6 +26,7 @@ module Quaerit
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.eager_load_paths << "#{Rails.root}/app/services"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
