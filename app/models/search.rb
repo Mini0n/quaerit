7184aff393cf73d12f99f2d6185e9173292fc167
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 02:52:06 -0500

    Singleton pattern dropped. Doesnt apply. Bad idea

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,25 +1,22 @@
-require 'singleton' # We'll only need ONE search class
+# frozen_string_literal: true
 
 class Search
-  include Singleton
-
   DEFAULT_ENGINE = 1 # google
 
-  attr_accessor :engines, :query, :offset #our class attributes
+  attr_accessor :engines, :query, :offset # our class attributes
 
-  def initialize(engines=[DEFAULT_ENGINE], query="", offset=0)
+  def initialize(engines = [DEFAULT_ENGINE], query = '', offset = 0)
     @engines = engines
     @query = URI.encode(query)
     @offset = offset
   end
 
   def search
-    "Searching with:\n #{self.inspect}"
+    "Searching with:\n #{inspect}"
   end
 
-  # get engine(s) as integers
+  # parse engines
   def engines=(engines)
     @engines = engines.scan(/\d+/).map(&:to_i)
   end
-
 end
