5ab5365fa113cbf113dad7bea1fec973845a8693
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 01:06:39 -0500

    Searches ctrl: - defaults | + search & about

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,51 +1,30 @@
 class SearchesController < ApplicationController
-  before_action :set_search, only: [:show, :update, :destroy]
+  before_action :set_search, only: [:search]
 
-  # GET /searches
-  def index
-    @searches = Search.all
+  def search
+    puts '-' * 43
+    puts params.inspect
+    puts '-' * 43
+    puts @search.inspect
+    puts '-' * 43
 
-    render json: @searches
+    render json: {'ola': 'k ase?'}
   end
 
-  # GET /searches/1
-  def show
-    render json: @search
+  def about
+    render plain: "Quearit\n\nSearch endpoint at: /search\n\nMore info in repo:\nhttps://github.com/Mini0n/quaerit"
   end
 
-  # POST /searches
-  def create
-    @search = Search.new(search_params)
 
-    if @search.save
-      render json: @search, status: :created, location: @search
-    else
-      render json: @search.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /searches/1
-  def update
-    if @search.update(search_params)
-      render json: @search
-    else
-      render json: @search.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /searches/1
-  def destroy
-    @search.destroy
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_search
-      @search = Search.find(params[:id])
+      @search = Search.instance
     end
 
     # Only allow a trusted parameter "white list" through.
     def search_params
-      params.require(:search).permit(:engine, :query, :offset)
+      params.permit(:engines, :query, :offset)
     end
 end
