e0a9ad77eab506cbd101ae847fff8149b836454b
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 02:22:04 -0500

    updated about to html, set Search attrs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,30 +1,38 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   before_action :set_search, only: [:search]
 
   def search
-    puts '-' * 43
-    puts params.inspect
+    # TODO: REMOVE THIS DEBUGING INFO OUTPUT
+
     puts '-' * 43
     puts @search.inspect
     puts '-' * 43
+    #
 
-    render json: {'ola': 'k ase?'}
+    render json: { 'ola': 'k ase?' }
   end
 
   def about
-    render plain: "Quearit\n\nSearch endpoint at: /search\n\nMore info in repo:\nhttps://github.com/Mini0n/quaerit"
+    title = "<h2>Quaerit</h2>"
+    route = "- Search at: <a href=\"/search?query=kabuto&offset=10&engines=1,2\">/search</a>"
+    github = "- More info at: <a href=\"http://github.com/Mini0n/quaerit\" target=\"_blank\">Quaerit repo</a>"
+
+    render html: "<pre>#{title}#{route}\n\n#{github}</pre>".html_safe
   end
 
-
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_search
-      @search = Search.instance
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def search_params
-      params.permit(:engines, :query, :offset)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  # get the Search singleton instance and set its attributes from search_params
+  def set_search
+    @search = Search.instance
+    search_params.to_h.each { |param, value| @search.send("#{param}=", value) }
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def search_params
+    params.permit(:engines, :query, :offset)
+  end
 end
