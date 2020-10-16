81833cd02e4046a857496c7c190b059e437af15d
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 03:41:50 -0500

    1st search approach & helper methods

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,6 +2,7 @@
 
 class Search
   DEFAULT_ENGINE = 1 # google
+  ENGINES = { 1 => 'Google', 2 => 'Bing' }.freeze
 
   attr_accessor :engines, :query, :offset # our class attributes
 
@@ -12,11 +13,73 @@ class Search
   end
 
   def search
-    "Searching with:\n #{inspect}"
+    puts "Searching with:\n #{inspect}"
+    puts '-- Searching'
+
+    return params_error unless valid_search? # check search will be valid
+
+    params = { query: @query, offset: @offset, engines: @engines.to_s }
+    results = []
+
+    @engines.each do |engine|
+      results << case engine
+                 when 1
+                   search_google(params)
+                 when 2
+                   search_bing(params)
+                 else
+                   engine_error(engine)
+      end
+    end
+
+    {
+      search: params,
+      results: results
+    }
+  end
+
+  def search_google(_params)
+    engine_results(1,
+                   [{ "title": 'cosa', "url": 'algo', "summary": 'stuff stuff' }])
+  end
+
+  def search_bing(_params)
+    engine_results(2,
+                   [{ "title": 'cosa', "url": 'algo', "summary": 'stuff stuff' }])
   end
 
   # parse engines
   def engines=(engines)
     @engines = engines.scan(/\d+/).map(&:to_i)
+    @engines = [DEFAULT_ENGINE] if @engines.empty? # use default engine if none
+  end
+
+  # parse offset
+  def offset=(offset)
+    @offset = offset.scan(/\d+/).map(&:to_i).first || 0
+  end
+
+  private
+
+  def valid_search?
+    return false if @engines.empty? || @query.empty?
+
+    true
+  end
+
+  def engine_results(engine, results)
+    { engine: "#{ENGINES[engine]} [#{engine}]", results: results }
+  end
+
+  def engine_error(engine)
+    search_error("No engine for code #{engine}")
+  end
+
+  def params_error
+    search_error("(Invalid || Empty) params: [engines:#{@engines}, query:#{@query}]")
+  end
+
+  def search_error(description)
+    { error: description }
   end
 end
