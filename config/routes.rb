a2ef40b89f8ff7400b06733263563e4eb115ef16
Eduardo PL <mini0n.000@gmail.com>
2020-10-16 01:07:01 -0500

    Added root & search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :searches
+  root 'searches#about'
+
+  get 'search', action: :search, controller: 'searches'
+
+  #resources :searches
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
