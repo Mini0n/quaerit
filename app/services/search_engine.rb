1b7e65b765e0393649373e62dfecc87f598c4606
Eduardo PL <mini0n.000@gmail.com>
2020-10-21 17:26:26 -0500

    better valid_url?, search->array, search_error {syms}

--- app/services/search_engine.rb
+++ app/services/search_engine.rb
@@ -8,7 +8,7 @@ class SearchEngine
   include HTTParty
 
   def search
-    { "error": 'Engine search not implemented' }
+    [{ "error": 'Engine search not implemented' }]
   end
 
   def self.name
@@ -16,12 +16,17 @@ class SearchEngine
   end
 
   def search_error(error)
-    { 'error': 'An error ocurred through the search process',
-      'details': error }
+    { error: 'An error ocurred through the search process',
+      details: error }
   end
 
   def valid_url?(url)
+    return false if url.empty?
+
     uri = URI.parse(URI.escape(url))
+
+    return false if uri.host.nil?
+
     uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
   rescue StandardError
     false
